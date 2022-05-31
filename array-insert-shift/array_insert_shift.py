a1fe3bdff33d2d9240bbbeaa6eacf699448ef741
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-05-31 23:00:09 +0300

    create array_insert_shift.py and update README file

--- array-insert-shift/array_insert_shift.py
+++ array-insert-shift/array_insert_shift.py
@@ -1,1 +1,14 @@
-print('code challenge 02')
+
+def array_insert_shift(array, number):
+    mid_arr = int(len(array)/2)
+    if len(array) == 0:
+        array.append(number)
+    elif len(array) % 2 == 0:
+        array.insert(mid_arr, number)
+    else:
+        array.insert(mid_arr+1, number)
+    return array
+
+
+
+
