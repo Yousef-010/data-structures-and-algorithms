2a8e08e02b7af80db8e121d36ac8965cca683a70
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-06-13 00:47:47 +0300

    finish code challenge 06

--- linked_list/README.md
+++ linked_list/README.md
@@ -19,6 +19,31 @@ A Linked List is a data structure represent the data as nodes each node contain
      - Arguments: none
      - Returns: a string representing all the values in the Linked List, formatted as:
        - "{ a } -> { b } -> { c } -> NULL"
+ <br/>
+**New code challenge** 
+   - append
+     - arguments: new value
+     - adds a new node with the given value to the end of the list
+   - insert before
+     - arguments: value, new value
+     - adds a new node with the given new value immediately before the first node that has the value specified
+   - insert after
+     - arguments: value, new value
+     - adds a new node with the given new value immediately after the first node that has the value specified
+   
+**New code challenge**  
+
+## Whiteboard Process
+- append function
+![append](append%20function%20.png)
+
+- insert before function
+![insert before](insert-before.png)
+
+- insert after function
+![insert after](insert-after.png)
+
+
 
 > ##### Approach & Efficiency
  - What approach did you take? why? 
@@ -30,7 +55,13 @@ A Linked List is a data structure represent the data as nodes each node contain
        - includes space complexity : BigO = O(1)
      - to_string time complexity : BigO = O(n)
        - to_string space complexity : BigO = O(1)
-
+<br/>
+     - insert_before time complexity : BigO = O(n)
+       - insert_before space complexity : BigO = O(n)
+     - insert_after time complexity : BigO = O(n)
+       - insert_after space complexity : BigO = O(n)
+     - append time complexity : BigO = O(n)
+        - append space complexity : BigO = O(n)
 
 > ##### API 
     - Insert
@@ -43,3 +74,18 @@ A Linked List is a data structure represent the data as nodes each node contain
     - to_string
         - it is a method to represent the collection of the values inside linkedList
           and return it in a spicific foramtted way 
+    - insert before 
+        it is a method will take tow arguments the first argument is the node to insert berfore it and the second argument is the new node 
+        head -> {1} -> {3} -> {2} -> X	
+        insert_before(3, 5)
+        head -> {1} -> {5} -> {3} -> {2} -> X
+    - insert after
+        it is a method will take tow arguments the first argument is the node to insert after it and the second argument is the new node 
+        head -> {1} -> {3} -> {2} -> X	
+        insert_before(3, 5)
+        head -> {1} -> {3} -> {5} -> {2} -> X
+    - append
+        it is a method take one argument (new node) append new node at theend of linked list 
+        head -> {1} -> {3} -> {2} -> X	
+        append(5)	
+        head -> {1} -> {3} -> {2} -> {5} -> X
--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -64,12 +64,90 @@ def test_includes_Not_exists_value(linked_list):
     assert actual == expected
 
 
-# def test_append_to_linked_list():
-# def test_append_multipleNode_to_linked_list():
-# def test_insert_before_to_linked_list():
-# def test_insert_at_the_beginning_to_linked_list():
-# def test_insert_after_to_linked_list():
-# def test_insert_at_the_last_to_linked_list():
+def test_append_to_linked_list(a_linked_list):
+    """
+    to add a node to the end of the linked list
+    """
+    a_linked_list.append(5)
+    assert a_linked_list
+
+
+def test_append_Multi_Node_to_linked_list(a_linked_list):
+    """
+    to add multiple nodes to the end of the linked list
+    """
+    a_linked_list.append(5)
+    a_linked_list.append(6)
+    a_linked_list.append(7)
+    assert a_linked_list
+
+
+def test_insert_before_middle_to_linked_list(i_linked_list):
+    """
+    to  insert a node before a node located i the middle of a linked list
+    """
+    i_linked_list.insert_before(3, 5)
+    assert i_linked_list
+
+
+def test_insert_before_first_to_linked_list(i_linked_list):
+    """
+    to insert a node before the first node of a linked list
+    """
+    i_linked_list.insert_before(1, 5)
+    assert i_linked_list
+
+
+def test_insert_before_to_linked_list(ll):
+    """
+    to insert before the first {2} in the linked_list
+    """
+    ll.insert_before(2, 5)
+    assert i_linked_list
+
+
+def test_insert_before_not_existing_Node_to_linked_list(i_linked_list):
+    """
+    to raise value error when insert before not existing Node
+    """
+    try:
+        i_linked_list.insert_before(4, 5)
+    except ValueError:
+        return ValueError == 'node with value 4 it is not founded'
+
+
+def test_insert_after_middle_to_linked_list(i_linked_list):
+    """
+    to insert after a node in the middle of the linked list
+    """
+    i_linked_list.insert_after(3, 5)
+    assert i_linked_list
+
+
+def test_insert_after_the_end_to_linked_list(i_linked_list):
+    """
+    to insert a node after the last node of the linked list
+    """
+    i_linked_list.insert_after(2, 5)
+    assert i_linked_list
+
+
+def test_insert_after_to_linked_list(ll):
+    """
+    to insert after the first {2} in the linked_list
+    """
+    ll.insert_after(2, 5)
+    assert i_linked_list
+
+
+def test_insert_after_not_existing_Node_to_linked_list(i_linked_list):
+    """
+    to raise value error when insert after not existing Node
+    """
+    try:
+        i_linked_list.insert_after(4, 5)
+    except ValueError:
+        return ValueError == 'node with value 4 it is not founded'
 
 
 def test_to_string(linked_list):
@@ -100,4 +178,29 @@ def linked_list():
     return linked_list
 
 
+@pytest.fixture
+def a_linked_list():
+    a_linked_list = LinkedList()
+    a_linked_list.append(1)
+    a_linked_list.append(3)
+    a_linked_list.append(2)
+    return a_linked_list
+
+
+@pytest.fixture
+def i_linked_list():
+    i_linked_list = LinkedList()
+    i_linked_list.append(1)
+    i_linked_list.append(3)
+    i_linked_list.append(2)
+    return i_linked_list
+
+
+@pytest.fixture
+def ll():
+    ll = LinkedList()
+    ll.append(1)
+    ll.append(2)
+    ll.append(2)
+    return ll
 
