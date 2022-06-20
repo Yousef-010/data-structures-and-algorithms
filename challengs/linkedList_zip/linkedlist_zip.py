3840298015169c9f77539ffaeda3fc83429a3725
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-06-20 09:39:23 +0300

    FINISH: code challenge 08 & update readme

--- challengs/linkedList_zip/linkedlist_zip.py
+++ challengs/linkedList_zip/linkedlist_zip.py
@@ -1,11 +1,80 @@
 from linked_list.linked_list import *
 
-first_linked_list = LinkedList()
-second_linked_list = LinkedList()
+
+def length_list(first_linked_list, second_linked_list):
+    """
+    this function to get the size of each linked list
+    """
+    counter1 = 0
+    counter2 = 0
+    cur1 = first_linked_list.head
+    cur2 = second_linked_list.head
+    while cur1:
+        counter1 += 1
+        cur1 = cur1.next
+    while cur2:
+        counter2 += 1
+        cur2 = cur2.next
+
+    return counter1, counter2
 
 
-def zip_list(first_linked_list, second_linked_list):
+def zip_lists(first_linked_list, second_linked_list):
+    """
+    this function takes 2 arguments: first_linked_list and second_linked_list as linked lists and return zipped linked list
+    with O(1) space complexity.
+    Arguments: 2 linked lists
+    Return:  Linked Lists zipped
+    """
 
-    pass
+    current1 = first_linked_list.head
+    current2 = second_linked_list.head
+    f_size = length_list(first_linked_list, second_linked_list)[0]
+    s_size = length_list(first_linked_list, second_linked_list)[1]
+
+    while True:
+        if current1 is None and current2 is None:
+            return 'your linked_lists id empty'
+
+        if current1 is None:
+            return second_linked_list
+
+        if current2 is None:
+            return first_linked_list
+
+        if f_size == s_size:
+            first_linked_list.insert_after(current1.value, current2.value)
+            current2 = current2.next
+            current1 = current1.next.next
+            if current1.next is None:
+                first_linked_list.append(current2.value)
+                return first_linked_list
+
+        if f_size < s_size:
+            second_linked_list.insert_before(current2.value, current1.value)
+            current2 = current2.next
+            current1 = current1.next
+            if current2.next is None:
+                return second_linked_list
+
+        if f_size > s_size:
+            first_linked_list.insert_after(current1.value, current2.value)
+            current2 = current2.next
+            current1 = current1.next.next
+            if current1.next is None:
+                return first_linked_list
 
 
+if __name__ == "__main__":
+    first_linked_list = LinkedList()
+    second_linked_list = LinkedList()
+
+    first_linked_list.append(1)
+    first_linked_list.append(3)
+    first_linked_list.append(2)
+
+    second_linked_list.append(5)
+    second_linked_list.append(9)
+    second_linked_list.append(4)
+
+    print(zip_lists(first_linked_list, second_linked_list).to_string())
--- tests/test_zip_linkedlist.py
+++ tests/test_zip_linkedlist.py
@@ -1,12 +1,87 @@
 import pytest
-from challengs.linkedList_zip import *
+
+from challengs.linkedList_zip.linkedlist_zip import zip_lists
+from linked_list.linked_list import LinkedList, Node
 
 
-def test_linked_list():
+def test_zip_linked_lists_if_both_linked_lists_is_empty():
+    # EMPTY linked list
+    first_linked_list = LinkedList()
+    # EMPTY linked list
+    second_linked_list = LinkedList()
+    actual = zip_lists(first_linked_list, second_linked_list)
+    expected = 'your linked_lists id empty'
+    assert actual == expected
+
+
+def test_zip_linked_lists_if_first_linked_list_is_empty():
+    # EMPTY linked list
+    first_linked_list = LinkedList()
+    # NOT EMPTY linked list
+    second_linked_list = LinkedList()
+    second_linked_list.append(1)
+    actual = zip_lists(first_linked_list, second_linked_list).to_string()
+    expected = second_linked_list.to_string()
+    assert actual == expected
+
+
+def test_zip_linked_lists_if_second_linked_list_is_empty():
+    # NOT EMPTY linked list
+    first_linked_list = LinkedList()
+    first_linked_list.append(1)
+    # EMPTY linked list
+    second_linked_list = LinkedList()
+    actual = zip_lists(first_linked_list, second_linked_list).to_string()
+    expected = first_linked_list.to_string()
+    assert actual == expected
+
+
+def test_zip_two_equal_length_linked_list(first_linked_list, second_linked_list):
     """
-    TEST ...
+    this is for test zipping with two equals length of linked list
     """
+    first_linked_list.append(2)
+    second_linked_list.append(4)
+    actual = zip_lists(first_linked_list, second_linked_list).to_string()
+    expected = '{1} -> {5} -> {3} -> {9} -> {2} -> {4} -> NULL'
+    assert actual == expected
 
-    pass
+
+def test_zip_linked_list_first_grater_than_second(first_linked_list, second_linked_list):
+    """
+    this is for test zipping with the first linked list is greater than the second_linked_list.
+    """
+    first_linked_list.append(2)
+    actual = zip_lists(first_linked_list, second_linked_list).to_string()
+    expected = '{1} -> {5} -> {3} -> {9} -> {2} -> NULL'
+    assert actual == expected
 
 
+def test_zip_linked_list_second_grater_than_first(first_linked_list, second_linked_list):
+    """
+        this is for test zipping with the second linked list is greater than the first linked list
+    """
+    second_linked_list.append(4)
+    actual = zip_lists(first_linked_list, second_linked_list).to_string()
+    expected = '{1} -> {5} -> {3} -> {9} -> {4} -> NULL'
+    assert actual == expected
+
+
+#############
+# fixtures
+#############
+
+@pytest.fixture
+def first_linked_list():
+    first_linked_list = LinkedList()
+    first_linked_list.append(1)
+    first_linked_list.append(3)
+    return first_linked_list
+
+
+@pytest.fixture
+def second_linked_list():
+    second_linked_list = LinkedList()
+    second_linked_list.append(5)
+    second_linked_list.append(9)
+    return second_linked_list
